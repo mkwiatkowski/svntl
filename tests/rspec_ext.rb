21b0b8aa08b2d8423587eac613f9c4ce4399b924
constant.beta <constant.beta@1cd7dad1-4c25-0410-b0e4-75a96f1fdbce>
2006-12-24 23:29:24 +0000

    Added 'Module.override!(method).with {}' notation to RSpec extensions.

--- tests/rspec_ext.rb
+++ tests/rspec_ext.rb
@@ -3,26 +3,33 @@
 # To make Kernel#system always return true, execute:
 #   Kernel.override!(:system).and_return true
 #
+# You may also use block which will substitute the original method:
+#   Kernel.override!(:system).with { |command| puts command }
+#
 # To restore the original Kernel#system, do:
 #   Kernel.restore! :system
 #
 
 class Module
   class Stub
-    def initialize procedure
+    def initialize &procedure
       @procedure = procedure
     end
 
     def and_return value
-      @procedure.call value
+      @procedure.call lambda { value }
+    end
+
+    def with &block
+      @procedure.call block
     end
   end
 
   def override! method
-    Stub.new(lambda do |value|
+    Stub.new do |procedure|
       alias_method "orig_#{method}", method
-      define_method(method) { value }
-    end)
+      define_method(method) { |*args| procedure.call(*args) }
+    end
   end
 
   def restore! method
