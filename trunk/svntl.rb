d3803e6172f2c477e9980513689fd1dfd0d7dd56
constant.beta <constant.beta@1cd7dad1-4c25-0410-b0e4-75a96f1fdbce>
2006-12-25 23:02:16 +0000

    Counting LOC of the whole project for all revisions in a repository.

--- trunk/svntl.rb
+++ trunk/svntl.rb
@@ -1,5 +1,34 @@
 require 'rexml/document'
 
+class Array
+  def each_cons length
+    each_index do |i|
+      if i + length > size
+        break
+      end
+
+      yield slice(i, length)
+    end
+  end
+end
+
+class REXML::Document
+  def inject_elements xpath, initial=nil
+    first = true
+
+    each_element(xpath) do |element|
+      if first and initial == nil
+        initial = element
+        first = false
+      else
+        initial = yield initial, element
+      end
+    end
+
+    initial
+  end
+end
+
 module SvnTimeline
 
   # Execute given command, returning its output on success.
@@ -14,24 +43,84 @@ module SvnTimeline
     return stdout
   end
 
+  class Revision
+    attr_reader :number
+    attr_accessor :loc
+
+    def initialize number
+      @number = number
+      @loc = 0
+    end
+  end
+
   class SubversionError < StandardError
   end
 
   class SubversionRepository
-    attr_reader :revisions
+    attr_reader :last_revision, :revisions
 
     def initialize url
       @url = url
-      get_info
+      @revisions = []
+
+      # Each repository have revision 0 with zero LOC.
+      @revisions << Revision.new(0)
+
+      # But we should remember that revision 0 is virtual
+      # and should not be counted.
+      class << @revisions
+        alias_method :orig_nitems, :nitems
+
+        def nitems
+          orig_nitems - 1
+        end
+      end
+
+      init_revisions
+    end
+
+    def revision number
+      @revisions.find { |rev| rev.number == number } or raise SubversionError, "No such revision."
     end
 
     private
-    def get_info
+    def get_loc_of_revision number
+      # Calling recursive ls to instantly get full list of files.
+      doc = REXML::Document.new execute_command("svn ls -R --xml -r#{number} #{@url}")
+
+      doc.inject_elements("/lists/list/entry[@kind='file']/name", 0) do |memo, path|
+        memo += execute_command("svn cat -r#{number} #{@url}/#{path.text}").to_a.size
+      end
+    end
+
+    def each_revision_pair
+      @revisions.each_cons(2) { |pair| yield(*pair) }
+    end
+
+    def init_revisions
       begin
-        doc = REXML::Document.new execute_command("svn info --xml #{@url}")
-        @revisions = Integer doc.elements["/info/entry"].attributes["revision"]
+        doc = REXML::Document.new execute_command("svn log --xml #{@url}")
+
+        doc.each_element("/log/logentry") do |logentry|
+          @revisions << Revision.new(logentry.attributes["revision"].to_i)
+        end
+
+        @revisions.sort! { |r1, r2| r1.number <=> r2.number }
+
+        each_revision_pair do |r1, r2|
+          begin
+            doc = execute_command("svn diff -r#{r1.number}:#{r2.number} --diff-cmd \"diff\" -x \"--normal\" #{@url}")
+            removed = doc.grep(/^</).size
+            added = doc.grep(/^>/).size
+            r2.loc = r1.loc + added - removed
+          rescue IOError
+            # Protect from situation when checking for file which didn't exist in rev. 0.
+            raise unless r1.number == 0
+            r2.loc = get_loc_of_revision r2.number
+          end
+        end
       rescue IOError
-        raise SubversionError
+        raise SubversionError, "No such repository."
       end
     end
   end
--- trunk/tests/subversion_repository_spec.rb
+++ trunk/tests/subversion_repository_spec.rb
@@ -4,34 +4,96 @@ require_gem 'builder'
 require 'svntl'
 include SvnTimeline
 
-# Imitate running "svn info --xml".
+class Array
+  def insert_at_random *obj
+    insert(rand(size+1), *obj)
+  end
+end
+
+class Integer
+  def lines_of_code
+    "a line of code\n" * self
+  end
+end
+
 module Spec::Runner::ContextEval::ModuleMethods
   def mock_svn options={}
     setup do
       SubversionRepository.override!(:execute_command).with do |command|
-        if command == 'svn info --xml file:///existing/repository'
-          options = { :revisions => 0 }.merge!(options)
-          revisions = options.values_at(:revisions)
+        options = { :loc => {}, :entries => Hash.new({}) }.merge!(options)
+        existing_root_url = 'file:///existing/repository'
+        existing_trunk_url = "#{existing_root_url}/trunk"
 
+        case command
+        when "svn diff -r0:1 --diff-cmd \"diff\" -x \"--normal\" #{existing_trunk_url}"
+            # "trunk/" didn't exist during rev. 0, thus we're raising an exception.
+            raise IOError
+        when /svn diff -r(\d+):(\d+) --diff-cmd "diff" -x "--normal" #{existing_trunk_url}/,
+             /svn diff -r(\d+):(\d+) --diff-cmd "diff" -x "--normal" #{existing_root_url}/
+          # Each repository have implicit revision no 0 with 0 LOC.
+          loc = options[:loc].merge!({ 0 => 0 })
+          loc_start, loc_end = loc[$1.to_i], loc[$2.to_i]
+
+          diff_document = ""
+
+          if loc_start != loc_end
+            loc_start.times { diff_document << "< what goes away\n" }
+            loc_end.times { diff_document << "> what goes in\n" }
+
+            # Insert few diff context lines to ensure that they're ignored.
+            rand(3).times { diff_document = diff_document.to_a.insert_at_random("1,2c1,6\n").to_s }
+            rand(3).times { diff_document = diff_document.to_a.insert_at_random("---\n").to_s }
+          end
+
+          diff_document
+        when "svn log --xml #{existing_trunk_url}",
+             "svn log --xml #{existing_root_url}"
           xml_document = ""
 
           xml = Builder::XmlMarkup.new :target => xml_document
           xml.instruct!
-          xml.info do
-            xml.entry :kind => "dir", :path => "repository", :revision => revisions do
-              xml.url "file:///existing/repository"
-              xml.repository do
-                xml.root "file:///existing/repository"
-                xml.uuid ""
-              end
-              xml.commit :revision => revisions do
+          xml.log do
+            options[:loc].to_a.sort.each do |rev|
+              rev = rev[0]
+
+              xml.logentry :revision => rev do
                 xml.author "ruby"
                 xml.date "2006-12-13T12:13:14.123456Z"
+                xml.msg "Bugfixes."
               end
             end
           end
 
           xml_document
+        when /svn ls -R --xml -r(\d+) (#{existing_trunk_url})/,
+             /svn ls -R --xml -r(\d+) (#{existing_root_url})/
+          url_used = $2
+          revision = $1.to_i
+          files_in_revision = options[:entries][revision].keys
+
+          xml_document = ""
+
+          xml = Builder::XmlMarkup.new :target => xml_document
+          xml.instruct!
+          xml.lists do
+            xml.list :path => url_used do
+              files_in_revision.each do |file|
+                xml.entry :kind => 'file' do 
+                  xml.name file
+                  xml.commit :revision => revision do
+                    xml.date "2006-12-13T12:13:14.123456Z"
+                  end
+                end
+              end
+            end
+          end
+
+          xml_document
+        when /svn cat -r(\d+) #{existing_trunk_url}\/(.*)/,
+             /svn cat -r(\d+) #{existing_root_url}\/(.*)/
+          filename = $2
+          revision = $1.to_i
+          options[:entries][revision][filename] or raise IOError
         else
           raise IOError
         end
@@ -47,8 +109,8 @@ end
 context "Non-existing repository" do
   mock_svn
 
-  specify "should raise SubversionError during creation" do
-    lambda { SubversionRepository.new "file:///do/not/exist" }.should_raise SubversionError
+  specify "should raise SubversionError(No such repository) during creation" do
+    lambda { SubversionRepository.new "file:///do/not/exist" }.should_raise SubversionError, "No such repository."
   end
 end
 
@@ -68,18 +130,175 @@ context "Empty repository" do
   end
 
   specify "should have zero revisions" do
-    @repo.revisions.should == 0
+    @repo.revisions.nitems.should == 0
   end
 end
 
 context "Repository with one revision" do
-  mock_svn :revisions => 1
+  mock_svn :loc => { 1 => 5 }
 
   setup do
     @repo = SubversionRepository.new "file:///existing/repository"
   end
 
   specify "should have one revision" do
-    @repo.revisions.should == 1
+    @repo.revisions.nitems.should == 1
+  end
+
+  specify "should return Revision object on revision(0)" do
+    @repo.revision(0).should_be_an_instance_of Revision
+  end
+
+  specify "should return Revision object on revision(1)" do
+    @repo.revision(1).should_be_an_instance_of Revision
+  end
+
+  specify "should raise SubversionError(No such revision) on revision(2)" do
+    lambda { @repo.revision(2) }.should_raise SubversionError, "No such revision."
+  end
+
+  specify "should report 0 LOC for revision(0)" do
+    @repo.revision(0).loc.should == 0
+  end
+
+  specify "should report 5 LOC for revision(1)" do
+    @repo.revision(1).loc.should == 5
+  end
+end
+
+context "Repository with three revisions" do
+  mock_svn :loc => { 1 => 5, 2 => 13, 3 => 7 }
+
+  setup do
+    @repo = SubversionRepository.new "file:///existing/repository"
+  end
+
+  specify "should have three revisions" do
+    @repo.revisions.nitems.should == 3
+  end
+
+  specify "should report 5 LOC for revision(1)" do
+    @repo.revision(1).loc.should == 5
+  end
+
+  specify "should report 13 LOC for revision(2)" do
+    @repo.revision(2).loc.should == 13
+  end
+
+  specify "should report 7 LOC for revision(3)" do
+    @repo.revision(3).loc.should == 7
+  end
+end
+
+context "Repository with five scattered revisions" do
+  mock_svn :loc => { 3 => 10, 4 => 46, 10 => 32, 12 => 32, 13 => 34 }
+
+  setup do
+    @repo = SubversionRepository.new "file:///existing/repository"
+  end
+
+  specify "should have five revisions" do
+    @repo.revisions.nitems.should == 5
+  end
+
+  specify "should report 10 LOC for revision(3)" do
+    @repo.revision(3).loc.should == 10
+  end
+
+  specify "should report 46 LOC for revision(4)" do
+    @repo.revision(4).loc.should == 46
+  end
+
+  specify "should report 32 LOC for revision(10)" do
+    @repo.revision(10).loc.should == 32
+  end
+
+  specify "should report 32 LOC for revision(12)" do
+    @repo.revision(12).loc.should == 32
+  end
+
+  specify "should report 34 LOC for revision(13)" do
+    @repo.revision(13).loc.should == 34
+  end
+
+  specify "should raise SubversionError(No such revision) on revision(1)" do
+    lambda { @repo.revision(1) }.should_raise SubversionError, "No such revision."
+  end
+
+  specify "should raise SubversionError(No such revision) on revision(5)" do
+    lambda { @repo.revision(5) }.should_raise SubversionError, "No such revision."
+  end
+end
+
+context "/trunk of repository with two revisions and two files" do
+  mock_svn :loc => { 1 => 12, 2 => 36 },
+           :entries => { 1 => { 'README' => 4.lines_of_code, 'code.py' => 8.lines_of_code } }
+
+  setup do
+    @repo = SubversionRepository.new "file:///existing/repository/trunk"
+  end
+
+  specify "should report 12 LOC for revision(1)" do
+    @repo.revision(1).loc.should == 12
+  end
+
+  specify "should report 36 LOC for revision(2)" do
+    @repo.revision(2).loc.should == 36
+  end
+end
+
+context "/trunk of repository with one revision and one file" do
+  mock_svn :loc => { 1 => 4 },
+           :entries => { 1 => { 'README' => 4.lines_of_code } }
+
+  setup do
+    @repo = SubversionRepository.new "file:///existing/repository/trunk"
+  end
+
+  specify "should report 4 LOC for revision(1)" do
+    @repo.revision(1).loc.should == 4
+  end
+end
+
+context "/trunk of repository with three empty revisions" do
+  mock_svn :loc => { 1 => 0, 2 => 0, 5 => 0 }
+
+  setup do
+    @repo = SubversionRepository.new "file:///existing/repository/trunk"
+  end
+
+  specify "should report 0 LOC for revision(1)" do
+    @repo.revision(1).loc.should == 0
+  end
+
+  specify "should report 0 LOC for revision(2)" do
+    @repo.revision(2).loc.should == 0
+  end
+
+  specify "should report 0 LOC for revision(5)" do
+    @repo.revision(5).loc.should == 0
+  end
+end
+
+context "Repository with three revisions of empty files" do
+  mock_svn :loc => { 1 => 0, 3 => 0, 5 => 0 },
+           :entries => { 1 => {},
+                         2 => { 'README' => '', 'INSTALL' => '' },
+                         3 => { 'README' => '', 'INSTALL' => '', 'code.py' => '' } }
+
+  setup do
+    @repo = SubversionRepository.new "file:///existing/repository"
+  end
+
+  specify "should report 0 LOC for revision(1)" do
+    @repo.revision(1).loc.should == 0
+  end
+
+  specify "should report 0 LOC for revision(3)" do
+    @repo.revision(3).loc.should == 0
+  end
+
+  specify "should report 0 LOC for revision(5)" do
+    @repo.revision(5).loc.should == 0
   end
 end
