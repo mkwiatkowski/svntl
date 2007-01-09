334a28d3de52733d8d2c580165843c3995142570
constant.beta <constant.beta@1cd7dad1-4c25-0410-b0e4-75a96f1fdbce>
2007-01-09 18:31:56 +0000

    Added :directory argument to generate_charts method.

--- tests/generate_charts_spec.rb
+++ tests/generate_charts_spec.rb
@@ -68,11 +68,36 @@ context "generate_charts method for existing repository" do
 
   specify "should accept `title` argument and pass it to each chart generation method" do
     Dir.stub!(:mkdir)
+
     @repository_object.should_receive(:chart_loc_per_day).once.with(:title => 'My Repository', :file => 'timeline/loc_per_day.png')
     @repository_object.should_receive(:chart_loc_per_day).once.with(:title => 'My Repository', :file => 'timeline/loc_per_day_small.png', :small => true)
     @repository_object.should_receive(:chart_loc_per_commit).once.with(:title => 'My Repository', :file => 'timeline/loc_per_commit.png')
     @repository_object.should_receive(:chart_loc_per_commit).once.with(:title => 'My Repository', :file => 'timeline/loc_per_commit_small.png', :small => true)
 
     generate_charts 'file:///existing/repository', :title => 'My Repository'
+  end
+
+  specify "should create named directory if `directory` argument was passed" do 
+    File.stub!(:exist?).and_return(false)
+    Dir.should_receive(:mkdir).with('svntl')
+
+    generate_charts 'file:///existing/repository', :directory => 'svntl'
+  end
+
+  specify "should call chart generation methods with `file` argument equal to `dir\\chart.png` in systems which has backslash as directory separator" do
+    File.stub!(:exist?).and_return(false)
+    (class << File; self; end).override!(:join).with do |*strings|
+      strings.join("\\")
+    end
+    Dir.stub!(:mkdir)
+
+    @repository_object.should_receive(:chart_loc_per_day).once.with(:file => 'dir\loc_per_day.png')
+    @repository_object.should_receive(:chart_loc_per_day).once.with(:file => 'dir\loc_per_day_small.png', :small => true)
+    @repository_object.should_receive(:chart_loc_per_commit).once.with(:file => 'dir\loc_per_commit.png')
+    @repository_object.should_receive(:chart_loc_per_commit).once.with(:file => 'dir\loc_per_commit_small.png', :small => true)
+
+    generate_charts 'file:///existing/repository', :directory => 'dir'
+
+    (class << File; self; end).restore! :join
   end
 end
