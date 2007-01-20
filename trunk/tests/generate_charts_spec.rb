f7eed3f3a1e8b296fa52bea5ddaeeb995f1296da
constant.beta <constant.beta@1cd7dad1-4c25-0410-b0e4-75a96f1fdbce>
2007-01-20 14:50:50 +0000

    Moved generate_html and generate_charts methods into SubversionRepository class.

--- trunk/tests/generate_charts_spec.rb
+++ trunk/tests/generate_charts_spec.rb
@@ -1,103 +1,75 @@
 require 'svntl'
 include SvnTimeline
 
-context "generate_charts method for non-existing repository" do
-  include ContextHelper
+context "Method generate_charts" do
+  mock_svn 'file:///existing/repository'
 
   setup do
-    SubversionRepository.stub!(:new).and_raise(SubversionError.new("No such repository."))
-  end
+    @repo = SubversionRepository.new 'file:///existing/repository'
 
-  specify "should not create any directory" do
-    Dir.should_not_receive(:mkdir)
-    without_exception(SubversionError) { generate_charts 'file:///do/not/exist' }
-  end
-
-  specify "should raise SubversionError(No such repository) during call" do
-    lambda { generate_charts 'file:///do/not/exist' }.should_raise SubversionError, "No such repository."
-  end
-end
-
-context "generate_charts method for existing repository" do
-  setup do
-    @repository_object = mock 'subversion_repository_object', :null_object => true
-    SubversionRepository.stub!(:new).and_return(@repository_object)
+    @gruff_line_object = mock "gruff_line_object", :null_object => true
+    Gruff::Line.stub!(:new).and_return(@gruff_line_object)
   end
 
   specify "should create directory timeline/ if repository exists" do
     File.stub!(:exist?).and_return(false)
     Dir.should_receive(:mkdir).with('timeline')
 
-    generate_charts 'file:///existing/repository'
+    @repo.generate_charts
   end
 
   specify "should not try to create existing timeline/ directory" do
     File.stub!(:exist?).and_return(true)
     Dir.should_not_receive(:mkdir)
 
-    generate_charts 'file:///existing/repository'
+    @repo.generate_charts
   end
 
-  specify "should once call chart_loc_per_commit with `file` argument set to `timeline/loc_per_commit.png`" do
+  specify "should call each chart generation method twice with `file` argument set to `timeline/loc_per_commit.png`, once setting `small` to true" do
     Dir.stub!(:mkdir)
-    @repository_object.should_receive(:chart_loc_per_commit).once.with(:file => 'timeline/loc_per_commit.png')
+    @repo.should_receive(:chart_loc_per_commit).once.with(:file => 'timeline/loc_per_commit.png')
+    @repo.should_receive(:chart_loc_per_commit).once.with(:file => 'timeline/loc_per_commit_small.png', :small => true)
+    @repo.should_receive(:chart_loc_per_day).once.with(:file => 'timeline/loc_per_day.png')
+    @repo.should_receive(:chart_loc_per_day).once.with(:file => 'timeline/loc_per_day_small.png', :small => true)
 
-    generate_charts 'file:///existing/repository'
-  end
-
-  specify "should once call chart_loc_per_commit with `file` argument set to `timeline/loc_per_commit_small.png` and `small` set to `true`" do
-    Dir.stub!(:mkdir)
-    @repository_object.should_receive(:chart_loc_per_commit).once.with(:file => 'timeline/loc_per_commit_small.png', :small => true)
-
-    generate_charts 'file:///existing/repository'
-  end
-
-  specify "should once call chart_loc_per_day with `file` argument set to `timeline/loc_per_day.png`" do
-    Dir.stub!(:mkdir)
-    @repository_object.should_receive(:chart_loc_per_day).once.with(:file => 'timeline/loc_per_day.png')
-
-    generate_charts 'file:///existing/repository'
-  end
-
-  specify "should once call chart_loc_per_day with `file` argument set to `timeline/loc_per_day_small.png` and `small` set to `true`" do
-    Dir.stub!(:mkdir)
-    @repository_object.should_receive(:chart_loc_per_day).once.with(:file => 'timeline/loc_per_day_small.png', :small => true)
-
-    generate_charts 'file:///existing/repository'
+    @repo.generate_charts
   end
 
   specify "should accept `title` argument and pass it to each chart generation method" do
     Dir.stub!(:mkdir)
 
-    @repository_object.should_receive(:chart_loc_per_day).once.with(:title => 'My Repository', :file => 'timeline/loc_per_day.png')
-    @repository_object.should_receive(:chart_loc_per_day).once.with(:title => 'My Repository', :file => 'timeline/loc_per_day_small.png', :small => true)
-    @repository_object.should_receive(:chart_loc_per_commit).once.with(:title => 'My Repository', :file => 'timeline/loc_per_commit.png')
-    @repository_object.should_receive(:chart_loc_per_commit).once.with(:title => 'My Repository', :file => 'timeline/loc_per_commit_small.png', :small => true)
+    @repo.should_receive(:chart_loc_per_day).once.with(:title => 'My Repository', :file => 'timeline/loc_per_day.png')
+    @repo.should_receive(:chart_loc_per_day).once.with(:title => 'My Repository', :file => 'timeline/loc_per_day_small.png', :small => true)
+    @repo.should_receive(:chart_loc_per_commit).once.with(:title => 'My Repository', :file => 'timeline/loc_per_commit.png')
+    @repo.should_receive(:chart_loc_per_commit).once.with(:title => 'My Repository', :file => 'timeline/loc_per_commit_small.png', :small => true)
 
-    generate_charts 'file:///existing/repository', :title => 'My Repository'
+    @repo.generate_charts :title => 'My Repository'
   end
 
   specify "should create named directory if `directory` argument was passed" do 
     File.stub!(:exist?).and_return(false)
     Dir.should_receive(:mkdir).with('svntl')
 
-    generate_charts 'file:///existing/repository', :directory => 'svntl'
+    @repo.generate_charts :directory => 'svntl'
   end
 
   specify "should call chart generation methods with `file` argument equal to `dir\\chart.png` in systems which has backslash as directory separator" do
-    File.stub!(:exist?).and_return(false)
-    File.metaclass.override!(:join).with do |*strings|
-      strings.join("\\")
+    begin
+      File.stub!(:exist?).and_return(false)
+      File.metaclass.override!(:join).with do |*strings|
+        strings.join("\\")
+      end
+      Dir.stub!(:mkdir)
+
+      @repo.should_receive(:chart_loc_per_day).once.with(:file => 'dir\loc_per_day.png')
+      @repo.should_receive(:chart_loc_per_day).once.with(:file => 'dir\loc_per_day_small.png', :small => true)
+      @repo.should_receive(:chart_loc_per_commit).once.with(:file => 'dir\loc_per_commit.png')
+      @repo.should_receive(:chart_loc_per_commit).once.with(:file => 'dir\loc_per_commit_small.png', :small => true)
+
+      @repo.generate_charts :directory => 'dir'
+
+    ensure
+      File.metaclass.restore! :join
     end
-    Dir.stub!(:mkdir)
-
-    @repository_object.should_receive(:chart_loc_per_day).once.with(:file => 'dir\loc_per_day.png')
-    @repository_object.should_receive(:chart_loc_per_day).once.with(:file => 'dir\loc_per_day_small.png', :small => true)
-    @repository_object.should_receive(:chart_loc_per_commit).once.with(:file => 'dir\loc_per_commit.png')
-    @repository_object.should_receive(:chart_loc_per_commit).once.with(:file => 'dir\loc_per_commit_small.png', :small => true)
-
-    generate_charts 'file:///existing/repository', :directory => 'dir'
-
-    File.metaclass.restore! :join
   end
 end
